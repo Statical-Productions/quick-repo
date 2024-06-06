22e1a37ea9a8a6b676d83245a6ba915fe6797374
St4t1c Cris <72564648+St4t1cCris@users.noreply.github.com>
2024-06-06 17:24:21 -0400

    Update https-generate.py

--- https-generate.py
+++ https-generate.py
@@ -8,13 +8,13 @@ import os
 # Variables
 API_TOKEN = os.environ.get("API_TOKEN")
 
-headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_TOKEN}" }
+hf_headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_TOKEN}" }
 
 # Functions
 def https_predict(url, session_hash, fn_index, data, headers):
     session = session_hash if session_hash is not None else str(uuid.uuid4())
     try:
-        response = requests.post(url, data=json.dumps({"data": data, "fn_index": fn_index, "session_hash": session}), headers=headers if headers is not None else {})
+        response = requests.post(url, data=json.dumps({"data": data, "fn_index": fn_index, "session_hash": session}), headers=headers if headers is not None else hf_headers or {})
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
